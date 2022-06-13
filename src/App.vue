976c95eea13cfb35f76d4d4f6d425b057053d045
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-06-13 18:14:01 +0200

    localstorage, filter, styling

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,52 @@ export default {
   data() {
     return {
       APP_STATUS: {
-
+        plants: [],
+        visumConfig: {
+          notDefined: true,
+          known: true,
+          unknown: true,
+        },
+        plantIndex: 0,
       },
       displayInfo: false,
-      plantIndex: 0,
       visibleSlide: 0,
-      plants: [],
       navBarActive: 1,
     };
   },
   computed: {
     slidesLen() {
-      return this.plants[this.plantIndex].imatges.length;
+      return this.filteredPlants[this.APP_STATUS.plantIndex].imatges.length;
     },
     knownPlants() {
-      return this.plants.filter(plant => plant.known);
+      return this.APP_STATUS.plants
+        .filter((plant) => plant.known)
+        .sort((a, b) => a.nom.localeCompare(b.nom));
     },
     unknownPlants() {
-      return this.plants.filter(plant => plant.known == false);
+      return this.APP_STATUS.plants
+        .filter((plant) => plant.known == false)
+        .sort((a, b) => a.nom.localeCompare(b.nom));
     },
     isThisPlantKnown() {
-      return this.plants[this.plantIndex].known == true;
+      return this.filteredPlants[this.APP_STATUS.plantIndex].known == true;
     },
     isKnownNull() {
-      return this.plants[this.plantIndex].known == null;
+      return this.filteredPlants[this.APP_STATUS.plantIndex].known == null;
+    },
+    filteredPlants() {
+      return this.APP_STATUS.plants.filter((plant) => {
+        let known = this.APP_STATUS.visumConfig.known
+          ? plant.known === true
+          : false;
+        let unknown = this.APP_STATUS.visumConfig.unknown
+          ? plant.known === false
+          : false;
+        let notDefined = this.APP_STATUS.visumConfig.notDefined
+          ? plant.known === null
+          : false;
+        return known || unknown || notDefined;
+      });
     },
   },
   methods: {
@@ -53,34 +75,66 @@ export default {
     },
     prevPlant() {
       this.visibleSlide = 0;
-      if (this.plantIndex <= 0) {
-        this.plantIndex = this.plants.length - 1;
+      if (this.APP_STATUS.plantIndex <= 0) {
+        this.APP_STATUS.plantIndex = this.filteredPlants.length - 1;
         return;
       }
 
-      this.plantIndex--;
+      this.APP_STATUS.plantIndex--;
+      this.updateLocalStorage();
     },
     nextPlant() {
       this.visibleSlide = 0;
-      if (this.plantIndex + 1 >= this.plants.length) {
-        this.plantIndex = 0;
+      if (this.APP_STATUS.plantIndex + 1 >= this.filteredPlants.length) {
+        this.APP_STATUS.plantIndex = 0;
         return;
       }
 
-      this.plantIndex++;
+      this.APP_STATUS.plantIndex++;
+      this.updateLocalStorage();
     },
     markAsKnown() {
-      this.plants[this.plantIndex].known = true;
+      this.filteredPlants[this.APP_STATUS.plantIndex].known = true;
+      this.updateLocalStorage();
     },
     markAsUnknown() {
-      this.plants[this.plantIndex].known = false;
-    }
+      this.filteredPlants[this.APP_STATUS.plantIndex].known = false;
+      this.updateLocalStorage();
+    },
+    updateLocalStorage() {
+      console.log("lsupdated");
+      localStorage.setItem("APP_STATUS", JSON.stringify(this.APP_STATUS));
+    },
   },
   mounted() {
-    this.plants = PlantsJson.plants
-      .map((value) => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => ({ ...value, known: null }));
+    if (localStorage.getItem("APP_STATUS") !== null) {
+      let local = JSON.parse(localStorage.getItem("APP_STATUS"));
+
+      this.APP_STATUS.plants = local.plants;
+      this.APP_STATUS.visumConfig = local.visumConfig;
+      this.APP_STATUS.plantIndex = local.plantIndex;
+    } else {
+      this.APP_STATUS.plants = PlantsJson.plants
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => ({ ...value, known: null }));
+
+      this.updateLocalStorage();
+    }
+  },
+  watch: {
+    "APP_STATUS.visumConfig.known"() {
+      this.APP_STATUS.plantIndex = 0;
+      this.updateLocalStorage();
+    },
+    "APP_STATUS.visumConfig.unknown"() {
+      this.APP_STATUS.plantIndex = 0;
+      this.updateLocalStorage();
+    },
+    "APP_STATUS.visumConfig.notDefined"() {
+      this.APP_STATUS.plantIndex = 0;
+      this.updateLocalStorage();
+    },
   },
 };
 </script>
@@ -89,16 +143,16 @@ export default {
   <header>
     <div class="nav-bar">
       <button @click="navBarActive = 0">Llista</button>
-      <button @click="navBarActive = 1">Visum</button>
-      <button @click="navBarActive = 2">[tercer]</button>
-      <span id="nav-select-color" :class="navBarActive == 0 ? '' : navBarActive == 1 ? 'second' : 'third'"></span>
+      <button class="title" @click="navBarActive = 1">Visum</button>
+      <button @click="navBarActive = 2">Config</button>
+      <span
+        id="nav-select-color"
+        :class="navBarActive == 0 ? '' : navBarActive == 1 ? 'second' : 'third'"
+      ></span>
     </div>
-    <h1 id="title">
-      <span>v</span><span>i</span><span>s</span><span>u</span><span>m</span>
-    </h1>
   </header>
 
-  <main v-if="plants.length != 0">
+  <main v-if="APP_STATUS.plants.length != 0">
     <div class="lista" v-show="navBarActive == 0">
       <div class="plantes-conegudes">
         <h2 class="title">Plantes conegudes</h2>
@@ -119,10 +173,16 @@ export default {
     </div>
 
     <div class="visum" v-show="navBarActive == 1">
-      <div class="card">
+      <div v-if="filteredPlants.length > 0" class="card">
         <Carousel @prevImage="prevImage" @nextImage="nextImage">
-          <CarouselSlide v-for="(imatge, index) in plants[plantIndex].imatges" :key="imatge" :index="index"
-            :visibleSlide="visibleSlide" :style="{ 'background-image': 'url(' + imatge + ')' }">
+          <CarouselSlide
+            v-for="(imatge, index) in filteredPlants[APP_STATUS.plantIndex]
+              .imatges"
+            :key="imatge"
+            :index="index"
+            :visibleSlide="visibleSlide"
+            :style="{ 'background-image': 'url(' + imatge + ')' }"
+          >
           </CarouselSlide>
         </Carousel>
         <transition name="info-card">
@@ -131,30 +191,49 @@ export default {
               <div class="info-group">
                 <h2 class="info-title">Nom</h2>
                 <p>
-                  <em>{{ plants[plantIndex].nom }}</em>
+                  <em>{{ filteredPlants[APP_STATUS.plantIndex].nom }}</em>
                 </p>
               </div>
               <div class="info-group">
                 <h2 class="info-title">Classificació</h2>
                 <p>
-                  <em>{{ plants[plantIndex].classificacio }}</em>
+                  <em>{{
+                    filteredPlants[APP_STATUS.plantIndex].classificacio
+                  }}</em>
                 </p>
               </div>
               <div class="info-group">
                 <h2 class="info-title">Característiques</h2>
-                <p v-for="carac in plants[plantIndex].caracteristiques" :key="carac">
+                <p
+                  v-for="carac in filteredPlants[APP_STATUS.plantIndex]
+                    .caracteristiques"
+                  :key="carac"
+                >
                   {{ carac }}
                 </p>
               </div>
             </div>
             <div class="info-card-buttons">
-              <button @click="markAsKnown" v-if="!isThisPlantKnown || isKnownNull">Marcar com coneguda</button>
-              <button @click="markAsUnknown" v-if="isThisPlantKnown || isKnownNull">Marcar com desconeguda</button>
+              <button
+                @click="markAsKnown"
+                v-if="!isThisPlantKnown || isKnownNull"
+              >
+                Marcar com coneguda
+              </button>
+              <button
+                @click="markAsUnknown"
+                v-if="isThisPlantKnown || isKnownNull"
+              >
+                Marcar com desconeguda
+              </button>
             </div>
           </div>
         </transition>
       </div>
-      <div class="buttons-container">
+      <div v-else>
+        <p>No hi han plantes per mostrar amb la configuració actual.</p>
+      </div>
+      <div class="buttons-container" v-if="filteredPlants.length > 0">
         <div class="row show-info">
           <button @click="displayInfo = !displayInfo">
             {{ displayInfo ? "Amaga Informació" : "Mostra Informació" }}
@@ -167,8 +246,28 @@ export default {
       </div>
     </div>
 
-    <div class="prueba" v-show="navBarActive == 2">
-      <h1>PROVA</h1>
+    <div class="config" v-show="navBarActive == 2">
+      <h2 class="title">Configuració</h2>
+      <div class="configuracio">
+        <p class="desc">Marca les opcions que vulguis veure al Visum!</p>
+        <div class="checkboxes">
+          <label>
+            <input
+              type="checkbox"
+              v-model="APP_STATUS.visumConfig.notDefined"
+            />
+            Mostra plantes sense definir
+          </label>
+          <label>
+            <input type="checkbox" v-model="APP_STATUS.visumConfig.known" />
+            Mostra plantes conegudes
+          </label>
+          <label>
+            <input type="checkbox" v-model="APP_STATUS.visumConfig.unknown" />
+            Mostra plantes desconegudes
+          </label>
+        </div>
+      </div>
     </div>
   </main>
 
@@ -199,12 +298,12 @@ footer {
 }
 
 header {
-  height: 15%;
-  justify-content: space-between;
+  height: 10%;
+  max-height: 60px;
 }
 
 .nav-bar {
-  min-height: 45px;
+  min-height: 100%;
   display: flex;
   width: 100%;
   position: relative;
@@ -218,25 +317,34 @@ header {
   cursor: pointer;
   outline: inherit;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   z-index: 10;
+}
+
+.nav-bar .title {
+  min-width: 50%;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 5px;
 }
 
 #nav-select-color {
   background-color: var(--secondary-color);
   height: 100%;
-  width: calc(100% / 3);
+  width: 25%;
   position: absolute;
   left: 0;
-  transition: left 0.2s;
+  transition: left 0.2s, width 0.2s;
 }
 
 #nav-select-color.second {
-  left: calc(100% / 3);
+  left: 25%;
+  width: 50%;
 }
 
 #nav-select-color.third {
-  left: calc(100% / 3 * 2);
+  left: 75%;
 }
 
 #title {
@@ -266,30 +374,32 @@ header {
 }
 
 main {
-  height: 85%;
+  height: 100%;
+  min-height: 90%;
+  max-height: calc(100% - 60px);
 }
 
 .visum,
 .lista,
-.prueba {
+.config {
   display: contents;
   width: 100%;
   height: 100%;
 }
 
 .lista {
-  display: inline-block;
+  display: block;
 }
 
-.lista>* {
+.lista > * {
   width: 100%;
   height: 50%;
   max-height: 50%;
   overflow-y: auto;
 }
 
-.plantes-conegudes>.title,
-.plantes-desconegudes>.title {
+.plantes-conegudes > .title,
+.plantes-desconegudes > .title {
   position: sticky;
   top: 0;
   background-color: var(--secondary-color);
@@ -297,11 +407,38 @@ main {
 
 .lista .title {
   font-size: 1.2rem;
-  padding: .5rem .8rem;
+  padding: 0.5rem 0.8rem;
 }
 
 .lista ul li:not(:last-child) {
-  margin-bottom: .2rem;
+  margin-bottom: 0.3rem;
+}
+
+.config {
+  display: block;
+}
+
+.config .title {
+  background-color: var(--secondary-color);
+  font-size: 1.2rem;
+  padding: 0.5rem 0.8rem;
+}
+
+.configuracio {
+  padding: 1rem;
+}
+
+.configuracio .desc {
+  font-style: italic;
+  margin-top: 0.5rem;
+}
+
+.checkboxes {
+  margin-top: 1rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
 }
 
 .info-card {
@@ -331,7 +468,7 @@ main {
   text-decoration: underline;
 }
 
-.info-card-buttons> :only-child {
+.info-card-buttons > :only-child {
   grid-column: 1/-1;
 }
 
